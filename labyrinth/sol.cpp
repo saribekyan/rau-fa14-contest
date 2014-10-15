cb4c60d867480b2736bf120fde7adbc801ebe876
Hayk Saribekyan <hayks@mit.edu>
2014-10-15 15:38:20 +0100

    Problem labyrinth is ready.

---
+++ labyrinth/sol.cpp
@@ -0,0 +1,87 @@
+#include <cstdio>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int l, r;
+vector < vector<int> > g;
+vector<int> mt;
+vector<char> used;
+ 
+bool try_kuhn (int v) {
+    if (used[v])  return false;
+    used[v] = true;
+    for (int i = 0; i < g[v].size(); ++i) {
+        int to = g[v][i];
+        if (mt[to] == -1 || try_kuhn(mt[to])) {
+            mt[to] = v;
+            return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    int m;
+    scanf("%d%d%d", &l, &r, &m);
+    g.resize(l);
+    vector<int> cl(l), cr(r);
+    for (int i = 0; i < l; ++i) {
+        cl[i] = i;
+    }
+    for (int i = 0; i < r; ++i) {
+        cr[i] = l + i;
+    }
+
+    while (m--) {
+        int p, q;
+        scanf("%d%d", &p, &q);
+        --p; --q;
+        g[p].push_back(q);
+        if (cl[p] == cr[q]) {
+            continue;
+        }
+        int c1 = cl[p];
+        int c2 = cr[q];
+        for (int i = 0; i < l; ++i) {
+            if (cl[i] == c1) {
+                cl[i] = min(c1, c2);
+            }
+        }
+        for (int i = 0; i < r; ++i) {
+            if (cr[i] == c2) {
+                cr[i] = min(c1, c2);
+            }
+        }
+    }
+
+    mt.assign(r, -1);
+    for (int v = 0; v < l; ++v) {
+        used.assign(l, false);
+        try_kuhn(v);
+    }
+
+    int comp = 0;
+    for (int i = 0; i < l + r; ++i) {
+        int cnt = 0;
+        for (int j = 0; j < l; ++j) {
+            if (cl[j] == i) {
+                ++cnt;
+            }
+        }
+        comp += (cnt != 0);
+        int in_matching = 0;
+        for (int j = 0; j < r; ++j) {
+            if (cr[j] == i && mt[j] != -1) {
+                ++in_matching;
+            }
+        }
+        if (cnt != in_matching) {
+            printf("Samvel\n");
+            return 0;
+        }
+    }
+    printf("Mihran\n");
+    return 0;
+}
+
---
+++ labyrinth/testgen.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+num = 1
+def add_test(g): # add params
+    global num
+    a = np.arange(1, g[0] + 1)
+    np.random.shuffle(a)
+    b = np.arange(1, g[1] + 1)
+    np.random.shuffle(b)
+    np.random.shuffle(g[2])
+    with open('tests/' + str(num).zfill(3), 'w') as f:
+        f.write('%d %d %d\n' % (g[0], g[1], len(g[2])))
+        for p in g[2]:
+            f.write('%d %d\n' % (a[p[0] - 1], b[p[1] - 1]))
+    print 'test', num, 'done'
+    num += 1
+
+def full(l, r):
+    return (l, r, np.transpose(np.vstack([np.arange(1, l + 1).repeat(r), np.tile(np.arange(1, r + 1), l)])))
+
+def empty(l, r):
+    return (l, r, np.array([], dtype='int64'))
+
+def concat(g1, g2):
+    return (g1[0] + g2[0], g1[1] + g2[1], np.vstack([g1[2], np.transpose(np.vstack([g2[2][:, 0] + g1[0], g2[2][:, 1] + g1[1]]))]))
+
+def random(l, r, m):
+    return (l, r, np.transpose(np.vstack([np.random.random_integers(1, l, m), np.random.random_integers(1, r, m)])))
+
+def path(n):
+    return (n, n, np.transpose(np.vstack([np.arange(n).repeat(2)[:-1], np.arange(n).repeat(2)[1:]])))
+
+add_test(full(1, 1))
+add_test(full(10, 10))
+add_test(full(9, 10))
+add_test(full(10, 9))
+add_test(full(100, 99))
+add_test(full(99, 100))
+add_test(path(100))
+add_test(concat(path(99), path(1)))
+
+add_test(concat(full(50, 90), random(49, 10, 300)))
+add_test(concat(full(60, 50), random(40, 50, 200)))
+
+add_test(concat(concat(concat(concat(concat(random(10, 14, 20),  random(13, 10, 100)), full(15, 9)), random(21, 13, 1)), random(9, 10, 5)),   full(10, 20)))
+add_test(concat(concat(concat(concat(concat(random(10, 14, 1000),  full(10, 13)),  random(9, 15, 1000)), random(13, 21, 900)), random(9, 10, 500)),  full(10, 20)))
+add_test(concat(concat(concat(concat(concat(random(10, 14, 200), random(13, 10, 9)),   full(15, 9)), random(21, 13, 1)), random(9, 10, 50)),  full(10, 20)))
+add_test(concat(concat(concat(concat(concat(path(10),  random(13, 10, 20)),  full(15, 9)), random(21, 13, 1)), random(9, 10, 100)), full(10, 20)))
+
+add_test(concat(random(50, 50, 1000), random(40, 48, 500)))
+add_test(concat(random(50, 50, 1000), random(40, 48, 500)))
+add_test(concat(random(50, 50, 1000), random(40, 48, 500)))
+add_test(concat(random(50, 50, 1000), random(40, 48, 500)))
+
+add_test(concat(random(50, 50, 5000), random(40, 48, 3000)))
+add_test(concat(full(50, 49), random(40, 48, 6000)))
+add_test(concat(random(50, 39, 6000), random(40, 48, 3000)))
+add_test(concat(random(50, 50, 3000), random(40, 48, 3000)))
+
