3b00328c5a983a3ea11c6d0c1c3dfb8ba2706331
Hayk Saribekyan <hayks@mit.edu>
2014-10-15 17:06:54 +0100

    Problem treasure is ready.

---
+++ treasure/interactor.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include "testlib.h"
+using namespace std;
+
+#define M 30000
+
+#define OK 0
+
+int main(int argc, char * argv[]) {
+    setName("interacts with user program");
+    registerInteraction(argc, argv);
+
+    int X = inf.readInt(0, 100);
+    int Y = inf.readInt(0, 100);
+    int n = inf.readInt(3, 100);
+    cout << X << ' ' << Y << '\n' << flush;
+
+    vector<double> x(n), y(n);
+    for (int i = 0; i < n; ++i) {
+        x[i] = inf.readDouble(0, X);
+        y[i] = inf.readDouble(0, Y);
+    }
+    inf.close();
+
+    x.push_back(x[0]);
+    y.push_back(y[0]);
+
+    double area = 0.0;
+    for (int i = 0; i < n; ++i) {
+        area += x[i] * y[i + 1] - x[i + 1] * y[i];
+    }
+    area = fabs(area);
+
+    for (int m = 0; m < M; ++m) {
+        double p = ouf.readDouble(0, X);
+        double q = ouf.readDouble(0, Y);
+
+        double tmp = 0.0;
+        for (int i = 0; i < n; ++i) {
+            tmp += fabs(p * y[i] + x[i] * y[i + 1] + x[i + 1] * q -
+                       (x[i] * q + y[i] * x[i + 1] + y[i + 1] * p));
+        }
+        if (fabs(tmp - area) < (1e-5)) {
+            cout << "Yes\n" << flush;
+            tout << 1;
+            return OK;
+        } else {
+            cout << "No\n" << flush;
+            tout << 0;
+        }
+    }
+    tout << 2;
+    return PE;
+}
+
---
+++ treasure/testgen.py
@@ -0,0 +1,68 @@
+import numpy as np
+import math
+
+num = 1
+def add_test(X, Y, polygon): # add params
+    global num
+    with open('tests/' + str(num).zfill(3), 'w') as f:
+        f.write('%d %d %d\n' % (X, Y, len(polygon)))
+        for p in polygon:
+            f.write('%.10lf %.10lf\n' % (p[0], p[1]))
+    print 'test', num, 'done'
+    num += 1
+
+def polygon(X, Y, x, y, r, n):
+    assert 0 <= x - r and x + r <= X
+    assert 0 <= y - r and y + r <= Y
+    assert r >= 1
+
+    alpha = 2 * math.pi / n
+
+    a = 0
+
+    xc = []
+    yc = []
+    for i in xrange(n):
+        xc.append(x + r * math.cos(a))
+        yc.append(y + r * math.sin(a))
+        a += alpha
+
+    return np.transpose(np.vstack([xc, yc]))
+
+add_test(100, 100, np.array([[41.1, 41.1],
+                             [41.1, 41.9],
+                             [90.1, 41.9],
+                             [90.1, 41.1]]))
+
+add_test(10, 10, np.array([[0, 0],
+                           [0, 10],
+                           [10, 10],
+                           [10, 0]]))
+
+add_test(100, 100, np.array([[0.1, 0.1],
+                             [0.1, 1.5],
+                             [0.9, 1.5],
+                             [0.9, 0.1]]))
+
+add_test(100, 100, np.array([[10.1, 20.1],
+                             [10.1, 21.5],
+                             [10.9, 21.5],
+                             [10.9, 20.1]]))
+
+add_test(100, 100, np.array([[90.1, 0.1],
+                             [90.1, 1.5],
+                             [90.9, 1.5],
+                             [90.9, 0.1]]))
+
+
+add_test(1, 1, np.array([[0, 0],
+                         [0, 1],
+                         [1, 1],
+                         [1, 0]]))
+
+add_test(100, 100, polygon(100, 100, 50, 50, 1, 5))
+add_test(100, 100, polygon(100, 100, 10, 10, 5, 10))
+add_test(100, 100, polygon(100, 100, 55, 52, 2, 3))
+add_test(100, 100, polygon(100, 100, 90, 80, 1, 5))
+add_test(100, 100, polygon(100, 100, 10, 95, 1, 5))
+
