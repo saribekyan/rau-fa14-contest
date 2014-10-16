cefff08f6350571a9ad77ab5bca7c984d0123645
Hayk Saribekyan <hayks@mit.edu>
2014-10-16 01:43:50 +0100

    Minor bug fix in the solution which was due to previous change.

--- lcm/sol.cpp
+++ lcm/sol.cpp
@@ -53,16 +53,16 @@ public:
 vector<int> get_primes() {
     vector<bool> is_prime(M + 1, true);
     is_prime[0] = is_prime[1] = false;
-    for (int i = 4; i <= 500000; i += 2) {
+    for (int i = 4; i <= M; i += 2) {
         is_prime[i] = false;
     }
     
     vector<int> primes;
     primes.push_back(2);
-    for (int i = 3; i * i <= 500000; i += 2) {
+    for (int i = 3; i * i <= M; i += 2) {
         if (is_prime[i]) {
             primes.push_back(i);
-            for (int j = i * i; j <= 500000; j += i) {
+            for (int j = i * i; j <= M; j += i) {
                 is_prime[j] = false;
             }
         }
